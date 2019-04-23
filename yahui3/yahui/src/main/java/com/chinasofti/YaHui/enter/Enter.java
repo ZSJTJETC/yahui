d59893856cc038915ed821462261f6e7838c5a62
zsjtjetc <1316311847@qq.com>
2019-04-23 18:35:16 +0800

    第三个亚惠版本客户端

---
+++ yahui3/yahui/src/main/java/com/chinasofti/YaHui/enter/Enter.java
@@ -0,0 +1,9 @@
+package com.chinasofti.YaHui.enter;
+
+import com.chinasofti.YaHui.control.Control;
+
+public class Enter {
+	public static void main(String[] args) {
+		new Control().start();
+	}
+}
